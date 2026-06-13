75b420ab62f62a4cafca3cef64e003556036d451
Amulya Parmar! TYG!! <parmar.amulya@gmail.com>
2026-06-13 00:30:26 -0400

    Fix new page prerender bailout

--- apps/web/app/(app)/new/page.tsx
+++ apps/web/app/(app)/new/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
+
 import { NewSessionFlow } from "./NewSessionFlow";
 
 export default function NewSessionPage() {
-  return <NewSessionFlow />;
+  return (
+    <Suspense fallback={null}>
+      <NewSessionFlow />
+    </Suspense>
+  );
 }
